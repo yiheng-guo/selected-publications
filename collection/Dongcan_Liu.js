02374a7020155acc93f42e6d0620e74618130915
GUO Yiheng <625236254@qq.com>
2026-02-26 09:33:41 +0800

    add a testing js

---
+++ collection/Dongcan_Liu.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        "title": "Occluded person re-identification via feature enhancement and contextual refinement.",
+        "date": "2026",
+        "authors": [
+            "Dongcan Liu",
+            "Linfeng Jiang"
+        ],
+        "venue": "Expert Systems with Applications",
+        "venueShort": "Expert Syst. Appl.",
+        "tags": [],
+        "awards": [],
+        "abstract": "",
+        "arxivUrl": "",
+        "paperUrl": "https://doi.org/10.1016/j.eswa.2025.130793",
+        "bibtex": "@article{DBLP:journals/eswa/LiuJ26,\n  author       = {Dongcan Liu and\n                  Linfeng Jiang},\n  title        = {Occluded person re-identification via feature enhancement and contextual\n                  refinement},\n  journal      = {Expert Syst. Appl.},\n  volume       = {304},\n  pages        = {130793},\n  year         = {2026},\n  url          = {https://doi.org/10.1016/j.eswa.2025.130793},\n  doi          = {10.1016/J.ESWA.2025.130793},\n  timestamp    = {Sun, 01 Feb 2026 13:38:19 +0100},\n  biburl       = {https://dblp.org/rec/journals/eswa/LiuJ26.bib},\n  bibsource    = {dblp computer science bibliography, https://dblp.org}\n}"
+    }
+]
