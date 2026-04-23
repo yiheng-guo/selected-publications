1692e90f8333a3ff7be37238ce17c9ae5006f85b
imethanguo <257661292+imethanguo@users.noreply.github.com>
2026-04-23 07:24:31 +0000

    chore: auto update publications

--- collection/auto-collected/auto_collected.js
+++ collection/auto-collected/auto_collected.js
@@ -997,5 +997,26 @@ module.exports = [
         "paperUrl": "https://doi.org/10.1109/ASE51524.2021.9678597",
         "bibtex": "@inproceedings{DBLP:conf/kbse/LiuWZ00C21,\n  author       = {Lu Liu and\n                  Lili Wei and\n                  Wuqi Zhang and\n                  Ming Wen and\n                  Yepang Liu and\n                  Shing{-}Chi Cheung},\n  title        = {Characterizing Transaction-Reverting Statements in Ethereum Smart\n                  Contracts},\n  booktitle    = {36th {IEEE/ACM} International Conference on Automated Software Engineering,\n                  {ASE} 2021, Melbourne, Australia, November 15-19, 2021},\n  pages        = {630--641},\n  publisher    = {{IEEE}},\n  year         = {2021},\n  url          = {https://doi.org/10.1109/ASE51524.2021.9678597},\n  doi          = {10.1109/ASE51524.2021.9678597},\n  timestamp    = {Fri, 16 May 2025 01:00:00 +0200},\n  biburl       = {https://dblp.org/rec/conf/kbse/LiuWZ00C21.bib},\n  bibsource    = {dblp computer science bibliography, https://dblp.org}\n}",
         "awards": []
+    },
+    {
+        "title": "Proceedings of the 15th Asia-Pacific Symposium on Internetware, Internetware 2024, Macau, SAR, China, July 24-26, 2024.",
+        "date": "2024",
+        "authors": [
+            "Hong Mei",
+            "Jian Lv",
+            "Abdelsalam Helal",
+            "Xiaoxing Ma",
+            "Shing-Chi Cheung",
+            "Jie Zhang",
+            "Tao Zhang"
+        ],
+        "venue": "",
+        "venueShort": "",
+        "abstract": "",
+        "tags": [],
+        "arxivUrl": "",
+        "paperUrl": "https://doi.org/10.1145/3671016",
+        "bibtex": "@proceedings{DBLP:conf/internetware/2024,\n  editor       = {Hong Mei and\n                  Jian Lv and\n                  Abdelsalam Helal and\n                  Xiaoxing Ma and\n                  Shing{-}Chi Cheung and\n                  Jie Zhang and\n                  Tao Zhang},\n  title        = {Proceedings of the 15th Asia-Pacific Symposium on Internetware, Internetware\n                  2024, Macau, SAR, China, July 24-26, 2024},\n  publisher    = {{ACM}},\n  year         = {2024},\n  url          = {https://doi.org/10.1145/3671016},\n  doi          = {10.1145/3671016},\n  timestamp    = {Tue, 20 Aug 2024 01:00:00 +0200},\n  biburl       = {https://dblp.org/rec/conf/internetware/2024.bib},\n  bibsource    = {dblp computer science bibliography, https://dblp.org}\n}",
+        "awards": []
     }
 ]
